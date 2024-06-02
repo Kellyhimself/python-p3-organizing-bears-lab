a1c534002b5898d15267dc797c3e629cf034ef6e
Kitui Kelly <kituikelly@gmail.com>
2024-06-02 12:49:24 +0300

    completes lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "F", "yellow", "medium", TRUE );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 8, "M", "yellow", "medium", TRUE );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 7, "F", "yellow", "medium", TRUE );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 8, "M", "yellow", "medium", TRUE );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 4, "F", "yellow", "medium", TRUE );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 5, "F", "yellow", "medium", FALSE );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 5, "M", "yellow", "medium", FALSE );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 4, "M", "yellow", "medium", FALSE );
